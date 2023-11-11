02ddba565990b13ed9204594f07e874d846143e2
Kirill <kiryusha.voronin.2004@list.ru>
2023-11-11 18:38:33 +0300

    new class Lion

--- app/src/main/java/com/example/lrndev/MainActivity.java
+++ app/src/main/java/com/example/lrndev/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lrndev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +17,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Cat milka = new Cat(0, "Milka", "'British'", "Grey");
-        milka.talk();
 
         Puma bagira = new Puma();
-        bagira.talk();
 
         Cat puma = new Puma();
-        puma.talk();
 
         Cat murzik = new Lion();
-        murzik.talk();
 
+        Lion lion = new Lion();
+
+        ArrayList<Cat> cats = new ArrayList<>();
+
+        cats.add(milka);
+        cats.add(bagira);
+        cats.add(puma);
+        cats.add(murzik);
+        cats.add(lion);
+
+        Cat cat = cats.get(0);
+        Cat cat1 = cats.get(1);
+        Cat cat2 = cats.get(2);
+        Cat cat3 = cats.get(3);
+        Cat cat4 = cats.get(4);
+
+        Puma puma1 = (Puma) cats.get(1);
+        puma1.talk();
+
+        Cat cat5 = cats.get(4);
+
+        if (cat5 instanceof Puma) {
+            Puma puma2 = (Puma) cat5;
+            puma2.talk();
+        } else {
+            Log.i("instanceof", "This is not Puma");
+        }
     }
 }
